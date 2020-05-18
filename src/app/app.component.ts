e0ba8e3700ad63e493e063b9e6b7998f845e6c5d
Adauto Freitas <c0512913@globalvale.onmicrosoft.com>
2020-05-18 01:16:07 -0300

    Adicionado o banco de dados e a classe quiz.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Quiz, Resposta } from './quiz';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,32 @@ export class AppComponent implements OnInit {
   title = 'QuizInAngular';
   telaInicial:boolean = false;
 
+  quiz:Quiz[] = [];
+
+  
+
   ngOnInit(): void {
+    
+    
     this.telaInicial = true;
+    let novoQuiz: Quiz = new Quiz();
+
+    let respostas:Resposta[] = [{titulo:"Pc 001", correta:true}, {titulo:"Pc 002", correta:false}];
+
+
+    novoQuiz.respostas = respostas;
+    novoQuiz.pergunta = "uma pergunta";
+
+    this.quiz.push(novoQuiz);
   }
 
   inicarQuiz():void
   {
     this.telaInicial = false;
+    console.log(this.quiz);
+
+    console.log(JSON.stringify({ data: this.quiz}, null, 4));
+
   }
   
 }
---
+++ src/app/quiz.ts
@@ -0,0 +1,11 @@
+export class Quiz
+{
+    pergunta:string;
+    respostas:Resposta[];
+
+}
+
+export class Resposta{
+    titulo:string;
+    correta:boolean;
+}
