87be4d1ada7a3c324cc6f2a5d1dcbf94f7dd1254
Adauto Freitas <c0512913@globalvale.onmicrosoft.com>
2020-05-21 01:46:45 -0300

    Correções e alteração na colocação da pergunta.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { ResultadoQuiz } from './resultadoQuiz';
 import { Quiz, Resposta } from './quiz';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
 
 
 @Component({
@@ -10,43 +12,80 @@ export class AppComponent implements OnInit {
 
   title = 'QuizInAngular';
   telaInicial: boolean = false;
-
-  quiz: Quiz;
-
-  checkButton:boolean = false;
+  listaQuiz: Quiz[] = [];
+  quizAtual: Quiz;
+  dataInicio: Date;
+  checkButton: boolean = false;
+  contadorProximaPergunta: number = 0;
+  resultadoQuiz: ResultadoQuiz = new ResultadoQuiz();
 
 
   ngOnInit(): void {
 
 
     this.telaInicial = true;
-    this.quiz = new Quiz();
+    let quiz = new Quiz();
 
-    let respostas: Resposta[] = [{ titulo: "Ré", correta: false, selecionada:false }, { titulo: "Mi", correta: false , selecionada:false},
-                                { titulo: "Fá", correta: false, selecionada:false },{titulo:"Dó", correta:false, selecionada:false},
-                                {titulo:"Dó", correta:false, selecionada:false}];
 
-    this.quiz.respostas = respostas;
-    this.quiz.pergunta = "Nos acordes de guitarra, o acorde C significa?";
-    this.quiz.quantidadeSelecaoResposta = 3;
+
+    let respostas: Resposta[] = [{ titulo: "Ré", correta: false, selecionada: false }, { titulo: "Mi", correta: false, selecionada: false },
+    { titulo: "Fá", correta: false, selecionada: false }, { titulo: "Dó", correta: false, selecionada: false }];
+
+    quiz.respostas = respostas;
+    quiz.pergunta = "Nos acordes de guitarra, o acorde C significa?";
+    quiz.quantidadeSelecaoResposta = 2;
+
+    this.listaQuiz[0] = quiz;
+    this.listaQuiz[1] = JSON.parse(JSON.stringify(quiz));
+    this.listaQuiz[1].pergunta = "Nos acordes de guitarra, o acorde C significa? 01";
+    this.listaQuiz[2] = JSON.parse(JSON.stringify(quiz));
+    this.listaQuiz[2].pergunta = "Nos acordes de guitarra, o acorde C significa? 02";
+    this.listaQuiz[3] = JSON.parse(JSON.stringify(quiz));
+    this.listaQuiz[3].pergunta = "Nos acordes de guitarra, o acorde C significa? 03";
 
   }
 
   inicarQuiz(): void {
     this.telaInicial = false;
-    console.log(this.quiz);
+    this.quizAtual = this.listaQuiz[this.contadorProximaPergunta];
+    console.log(this.quizAtual);
 
-    console.log(JSON.stringify({ data: this.quiz }, null, 4));
+
+    this.dataInicio = new Date();
+
+    console.log(JSON.stringify({ data: this.listaQuiz }, null, 4));
 
   }
-  desabilitaSelecao():boolean
-  {
-    return this.quiz.respostas.filter( resposta => resposta.selecionada == true).length >= this.quiz.quantidadeSelecaoResposta;
+  desabilitaSelecao(): boolean {
+    return this.quizAtual.respostas.filter(resposta => resposta.selecionada == true).length >= this.quizAtual.quantidadeSelecaoResposta;
   }
 
-  proximaPergunta():void
-  {
-    console.log(JSON.stringify({ data: this.quiz }, null, 4));
+  proximaPergunta(): void {
+    console.log(JSON.stringify({ data: this.quizAtual }, null, 4));
+    if(this.listaQuiz.length != (this.contadorProximaPergunta +1) )
+    {
+      this.quizAtual = this.listaQuiz[++this.contadorProximaPergunta];
+    }
+    else
+    {
+      this.finalizaQuiz();
+    }
+    
+
+
+  }
+
+ finalizaQuiz(): void {
+    this.resultadoQuiz.duracaoEmMs = new Date().getTime() - this.dataInicio.getTime();
+    this.resultadoQuiz.quatidadeAcertos = this.listaQuiz.filter(quiz =>
+      quiz.respostas.filter(resposta => resposta.correta && resposta.selecionada).length>0).length;
+
+
+    this.resultadoQuiz.quatidadeErros = this.listaQuiz.filter(quiz =>
+      quiz.respostas.filter(
+        resposta => !resposta.correta && resposta.selecionada).length>0).length;
+    
+        console.log(JSON.stringify({ data: this.resultadoQuiz }, null, 4)); 
   }
 
 }
---
+++ src/app/resultadoQuiz.ts
@@ -0,0 +1,8 @@
+export class ResultadoQuiz
+{
+    duracaoEmMs:number
+    quatidadeAcertos:number;
+    quatidadeErros:number;
+    foiAprovado:boolean;
+
+}
